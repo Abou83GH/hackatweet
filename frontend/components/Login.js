7f4ab5691d058261d2b43126aa3d791f8e7fb5b5
Aboubakar Yatera <yatera.abou@gmail.com>
2023-11-23 17:35:29 +0100

    HCKT : Frontend Signin Comp Done

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,28 +1,80 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import Head from "next/head";
-import { Modal } from "antd";
+import { Modal, Button } from "antd";
 import styles from "../styles/Login.module.css";
 
 function Login() {
+  const [isSignInModalOpen, setIsSignInModalOpen] = useState(false);
+  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
+
+  const showSignInModal = () => {
+    setIsSignInModalOpen(true);
+  };
+
+  const showSignUpModal = () => {
+    setIsSignUpModalOpen(true);
+  };
+
+  const handleSignUpBtn = () => {
+    setIsSignUpModalOpen(false);
+  };
+
+  const handleSignInBtn = () => {
+    setIsSignInModalOpen(false);
+  };
+
+  const handleSUCancel = () => {
+    setIsSignUpModalOpen(false);
+  };
+
+  const handleSICancel = () => {
+    setIsSignInModalOpen(false);
+  };
+  
+
   return (
     <div className={styles.loginContainer}>
       <div className={styles.container}>
-      <div className={styles.leftsection} >
+        <div className={styles.leftsection}>
           <div className={styles.logobig}>
             <img src="logo.png" alt="Logo gros" />
           </div>
         </div>
         <div className={styles.rightsection}>
           <img src="logo.png" alt="Logo petit" />
-          <h1>Titre Principal</h1>
-          <h2>Sous-titre</h2>
+          <h1>See what's happening</h1>
+          <h2>Join hackatweet today</h2>
           <div className={styles.content}>
-            <div className={styles.component}>-- Votre composant ici --</div>
-            <p className={styles.paragraph}>Votre paragraphe ici.</p>
-            <div className={styles.component}>-- Autre composant ici --</div>
+            <Button className={styles.component} onClick={showSignUpModal}>
+              Sign up
+            </Button>
+            <Modal
+              title="Basic Modal"
+              open={isSignUpModalOpen}
+              onOk={handleSignUpBtn}
+              onCancel={handleSUCancel}
+            >
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </Modal>
+            <p className={styles.paragraph}>Already have an account</p>
+            <Button className={styles.component} onClick={showSignInModal}>
+              Sign in
+            </Button>
+            <Modal
+              title="Basic Modal"
+              open={isSignInModalOpen}
+              onOk={handleSignInBtn}
+              onCancel={handleSICancel}
+            >
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </Modal>
           </div>
         </div>
-       
       </div>
     </div>
   );
--- frontend/components/Signin.js
+++ frontend/components/Signin.js
@@ -1,9 +1,43 @@
-import { useSelector } from "react-redux";
-import Head from "next/head";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect} from 'react'
+import { login } from '../reducers/user';
 import { Modal } from "antd";
 import styles from "../styles/Login.module.css";
 
 function Signin() {
+    const dispatch = useDispatch();
+	const user = useSelector((state) => state.user.value);
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [signInUsername, setSignInUsername] = useState('');
+	const [signInPassword, setSignInPassword] = useState('');
+
+
+    const handleConnection = () => {
+
+		fetch('http://localhost:3000/users/signin', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ username: signInUsername, password: signInPassword }),
+		}).then(response => response.json())
+			.then(data => {
+				if (data.result) {
+					dispatch(login({ username: signInUsername, token: data.token }));
+					setSignInUsername('');
+					setSignInPassword('');
+					setIsModalVisible(false)
+				}
+			});
+	};
+
+
+    return (
+        <div >
+			<button className={styles.signInButton}>
+				Sign in
+			</button>
+		</div>
+    )
 
 }
 export default Signin;
